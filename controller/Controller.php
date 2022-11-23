826d60a382bfab3c913d199dfdfc2da72d83fac4
JoaoEnrique <jebsantosalves@gmail.com>
2022-11-22 22:05:30 -0300

    Conexao DB

--- controller/Controller.php
+++ controller/Controller.php
@@ -1,6 +1,6 @@
 <?php
 $root = realpath($_SERVER["DOCUMENT_ROOT"]);
-require_once("$root\proj-IBM\model\cadastrar.php");
+require_once("$root\sjm-assistance\model\cadastrar.php");
 
 class Controller{
 
